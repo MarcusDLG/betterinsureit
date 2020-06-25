5e305a2eb61707d79d758e64b154b591c8521fac
MatthewSDick <matthew.dick@outlook.com>
2020-06-25 16:20:32 -0400

    Basic button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+//import logo from "./logo.svg";
+import "./App.scss";
+import Dropdown from "./Dropdown";
+
+// This will be replaced with info from Cloudinary
+const items = [
+  {
+    id: 1,
+    value: "Speak to a Professional",
+  },
+  {
+    id: 2,
+    value: "Life Insurance",
+  },
+  {
+    id: 3,
+    value: "Vision Insurance",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="BetterInsureItLogoSize.png" alt="logo" />
-        <p>Better Insure IT</p>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <div className="App">
+          <header className="App-header">
+            <img src="BetterInsureItLogoSize.png" alt="logo" />
+            <p>Better Insure IT</p>
+          </header>
+        </div>
+        <div>
+          {/* Take out multiSelect for only one choice */}
+          <Dropdown title="Individuals & Families" items={items} multiSelect />
+        </div>
+      </div>
+    </>
   );
 }
 
